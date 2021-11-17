0e7e6dae324e365866bff1ae96415fc255f4bc5f
TimKooistra <154630@csgaugustinus.nl>
2021-11-17 09:08:18 +0000

    x

--- JS/H1/H1O03.js
+++ JS/H1/H1O03.js
@@ -8,11 +8,11 @@ function setup() {
 
 function draw() {
   noStroke();
-  // fill(0,0,255,0.3);
+  fill(0,0,255,0.3);
   fill('red');
   rect(0,150,450,150);
-  fill('green');
+  fill(0,128,0);
   rect(0,0,150,450);
-  fill('blue');  
+  fill(0,0,255);  
   rect(75,0,375,450);
 }
